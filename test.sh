4abc438c604c90b0512609c34738610a3f439533
Davide Bortolami <d@fermiumlabs.com>
2016-11-19 21:10:04 +0100

    reverse other erroneus commits

--- test.sh
+++ test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
+printf "\n\ninstalling timelion\n"
+./kibana/bin/kibana plugin --install elastic/timelion
+
 printf "\n\ninstalling the plugin\n"
-./kibana/bin/kibana-plugin install $(./transfer upload target/mathlion-${KIBANA_VERSION}.zip)
+./kibana/bin/kibana plugin --install mathlion -u $(./transfer upload target/mathlion-*.zip)
 
 printf "\n\nstarting kibana in the background\n"
-nohup bash -c "./kibana/bin/kibana --no-ssl >&1 &"
+nohup bash -c "./kibana/bin/kibana>&1 &"
 
 printf "\n\nWaiting for 120 seconds...\n"
 sleep 120
